666160ca68b6a5251a6be1b43b5b190929df4549
Shary <shaharyarsajid@gmail.com>
2023-02-10 17:55:01 +0500

    Fixed a bug where zero was not being accepted as an input.

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,12 +3,12 @@ from wtforms import StringField, RadioField, IntegerField
 from wtforms.validators import DataRequired, InputRequired, NumberRange
 
 class InputForm(FlaskForm):
-    firstName = StringField('First Name', validators=[DataRequired()])
-    lastName = StringField('Last Name', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
-    experience = IntegerField('Experience', validators=[DataRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
-    grade = IntegerField('Grade', validators=[DataRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
-    last_promotion = IntegerField('Last Promotion', validators=[DataRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
+    firstName = StringField('First Name', validators=[InputRequired()])
+    lastName = StringField('Last Name', validators=[InputRequired()])
+    age = IntegerField('Age', validators=[InputRequired(), NumberRange(min=-0, message='Enter a number greater than 0')])
+    experience = IntegerField('Experience', validators=[InputRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
+    grade = IntegerField('Grade', validators=[InputRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
+    last_promotion = IntegerField('Last Promotion', validators=[InputRequired(), NumberRange(min=0, message='Enter a number greater than 0')])
     promo1 = RadioField('Promotion 1', choices=[
         ('M', 'M'),
         ('E', 'E'),
